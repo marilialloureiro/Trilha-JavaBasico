ed4bf02e66af2ba93e1508d5965c699de9b9f7ac
Marilia <marilialloureiro@hotmail.com>
2024-02-21 13:52:44 -0300

    Executando 1 projeto

--- Projeto1/Sistema-SmartTV/src/SmartTv.java
+++ Projeto1/Sistema-SmartTV/src/SmartTv.java
@@ -1,3 +1,13 @@
 public class SmartTv {
     
+    /*PROJETO - Criar uma classe para representar uma Smart TV, onde:
+     * 1. Ela tenha caracteristicas: ligada (boolean), canal (int) e volume (int)
+     * 2. A TV poderá lugar e desligar mudando seu estado
+     * 3. A TV poderá aumentar e dominuir o volume sempre +1 ou -1
+     * 4. A TV poderá mudar de candal de 1 em 1, ou definindo o numero correspondente*/
+
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
 }
