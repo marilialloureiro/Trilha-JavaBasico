21ef9e5c8cd972882e080c71025017f1007c3845
Marilia <marilialloureiro@hotmail.com>
2024-02-21 14:14:52 -0300

    Projeto 1 - criando métodos

--- Projeto1/Sistema-SmartTV/src/SmartTv.java
+++ Projeto1/Sistema-SmartTV/src/SmartTv.java
@@ -9,5 +9,33 @@ public class SmartTv {
     boolean ligada = false;
     int canal = 1;
     int volume = 25;
+    
+    //Criando métodos
 
+    public void ligar(){
+        ligada = true;
+    }
+
+    public void desligar(){
+        ligada = false;
+    }
+
+    public void aumentarVolume(){
+        volume ++;
+    }
+
+    public void diminuirVolume(){
+        volume --;
+    }
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void aumentarCanal(){
+        canal ++;
+    } 
+    public void diminuirCanal(){
+        canal --;
+    } 
 }
--- Projeto1/Sistema-SmartTV/src/Usuario.java
+++ Projeto1/Sistema-SmartTV/src/Usuario.java
@@ -8,5 +8,25 @@ public class Usuario {
         System.out.println("Canal atual: " + smartTv.canal);
         System.out.println("Volume atual: "+ smartTv.volume);
 
+        smartTv.ligar();
+        System.out.println("Novo status -> TV ligada? " + smartTv.ligada);
+
+        //ações teste
+        smartTv.diminuirVolume(); //24
+        smartTv.diminuirVolume(); //23
+        smartTv.diminuirVolume(); //22
+        smartTv.aumentarVolume(); //23
+        System.out.println("Volume atual: " + smartTv.volume);
+
+      
+        //mudando o canal
+        System.out.println("Canal atual: " + smartTv.canal); //validação
+        smartTv.mudarCanal(10); //mudança
+        System.out.println("Novo canal: " + smartTv.canal);
+
+        smartTv.aumentarCanal();
+        System.out.println("Novo canal: " + smartTv.canal);
+
+
     }
 }
